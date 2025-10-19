ae80ce896035350b1501247d5c4aed7f6bfaa329
Muhammad Raheem Khan <muhammadraheemk96@gmail.com>
2025-10-19 23:03:16 +0100

    Fixex the User Session type in Express Request

--- src/types/express-session.d.ts
+++ src/types/express-session.d.ts
@@ -1,15 +1,16 @@
 import 'express-serve-static-core';
 import type { UserSession } from '@thallesp/nestjs-better-auth';
+import { Session } from 'src/lib/betterauth';
 
 declare module 'express-serve-static-core' {
   interface Request {
     /**
      * Better Auth session attached by the AuthGuard
      */
-    session?: UserSession;
+    session?: Session;
     /**
      * Convenience reference to the session user (Better Auth attaches this too)
      */
-    user?: UserSession['user'];
+    user?: Session['user'];
   }
 }
